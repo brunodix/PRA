1d210edaa82f3095bda749d24be835c0bacb3b3c
Bruno Dix <brunodix@gmail.com>
2015-03-14 10:51:17 -0300

    Alteração nos métodos e implementações

--- FILA/queue.h
+++ FILA/queue.h
@@ -5,18 +5,26 @@ template <class T>
 class QueueNode {
 	T value;
 	QueueNode<T> *next;
-	///Referencia do ultimo para agilizar inserção
-	QueueNode<T> *last;
 	public:
 	QueueNode(T _value) { value = _value; };
+	void setNext(QueueNode<T> *node) { next = node;  };
+	QueueNode<T>* getNext() { return next;  };
 };
 
 template <class NT>
 class Queue {
 	QueueNode<NT> *first;
+	///Referencia do ultimo para agilizar inserção
+	QueueNode<NT> *last;
 	public:
-		void add(QueueNode<NT> *node) {};
-		QueueNode<NT>* get() {};
+		void queue(QueueNode<NT> *node) {
+			if (first) {
+				last->setNext(node);
+				last = node;
+			}
+
+		};
+		QueueNode<NT>* dequeue() {};
 };
 
 #endif
