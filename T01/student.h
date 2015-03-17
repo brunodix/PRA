6a912a4765fc5217e6f2361ddbddcf78128cf902
Bruno Dix <brunodix@gmail.com>
2015-03-17 12:06:59 -0300

    Inclusão de documentação.

--- T01/student.h
+++ T01/student.h
@@ -6,9 +6,15 @@
 #define STUDENT_H
 
 
+/**
+ * Classe que representa uma matricula de um usuário, é a estrutura armazenada no arquivo.
+ */
 class Student {
 private:
 public:
+    /**
+     * Construtor que recebe os principais parâmetros.
+     */
     Student(int _enrollNumber, std::string _name, double *_score, int _absence);
     Student(){};
 
@@ -27,6 +33,9 @@ private:
     StudentStatus status;
 
 public:
+    /**
+     * Método que retorna um representação em std::string do objeto.
+     *;
     std::string toString();
 
 };
