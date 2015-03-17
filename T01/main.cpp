79756c9332c2834610445b692b5aca84706af4ef
Bruno Dix <brunodix@gmail.com>
2015-03-17 19:32:02 -0300

    atualizando documentação

--- T01/main.cpp
+++ T01/main.cpp
@@ -15,48 +15,53 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+    /**
+     * Carrega os argumentos da linha de comando.
+     */
     long pageSize = atoi(argv[1]);
     long size = atoi(argv[2]);
 
-    cout << "Pagina:" << pageSize << endl;
-    cout << "Tamanho:" << size << endl;
+    cout << "Quantidade de registros da Pagina:" << pageSize << endl;
+    cout << "Tamanho do arquivo gerado em MiB:" << size << endl;
 
-    FILE *f = fopen("/tmp/t.bin", "wb+");
-    /// Descobre quantos registros são necessário para atingir o tamanho
-    /// tamanho em Mebibytes
+    FILE *f = fopen("data.bin", "wb+");
+    /**
+     * Descobre quantos registros são necessário para atingir o tamanho
+     * tamanho em Mebibytes
+     */
     long regNum = (size * 1024 * 1024) / sizeof(Student);
     cout << regNum << endl;
     // Calcula as iterações com base do numero de elementos e páginas
     long iterations = regNum / pageSize;
     long remaining = regNum % pageSize;
-	time_t start,end;
+    time_t start,end;
     StudentFactory *factory = new StudentFactory();
 
-	time (&start);
+    time (&start);
 
+    /// Executa a gravação
     for (int i = 0; i < iterations; i++) {
         writeElements(f, pageSize, factory);
     }
+    /// Grava o resto da quantidade de registros
     writeElements(f, remaining, factory);
 
-	time (&end);
-	cout << "Gravação:" << difftime (end,start) << endl;
-
-    //delete(factory);
+    time (&end);
+    cout << "Tempo de Gravação:" << difftime (end,start) << endl;
 
     rewind(f);
 
-	time (&start);
+    time (&start);
 
-	for (int i = 0; i < iterations; i++) {
-		readElements(f, pageSize);
-	}
-	readElements(f, remaining);
+    /// Executa a gravação
+    for (int i = 0; i < iterations; i++) {
+        readElements(f, pageSize);
+    }
+    /// Le o que sobrou dos registros
+    readElements(f, remaining);
 
-
-	time (&end);
-	cout << "Leitura:" << end -start << endl;
-
+    time (&end);
+    cout << "Tempo de Leitura:" << end -start << endl;
 
     fclose(f);
 
