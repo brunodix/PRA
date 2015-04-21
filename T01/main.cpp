03216d6f49bcd9667e1eed45064bb345580754aa
Bruno Dix <brunodix@gmail.com>
2015-04-21 16:44:36 -0300

    Alteração no calculo do número de registros para evitar tratar os registros restantes.

--- T01/main.cpp
+++ T01/main.cpp
@@ -11,16 +11,26 @@ void writeElements(FILE *f, long size, StudentFactory *factory);
 
 void readElements(FILE *f, long size);
 
-void writeFile(FILE *f, long iterations, long remaining, StudentFactory *factory);
+void writeFile(FILE *f, long iterations, StudentFactory *factory);
 
-void readFile(FILE *f, long iterations, long remaining);
+void readFile(FILE *f, long iterations);
 
 using namespace std;
 
+//Tamanho do arquivo em MEGAS
+static int const SIZE = 10;
+// 1 MiB
 static int const MIB = 1024 * 1024;
-static int const FILE_SIZE = MIB*10;
+// Número de vias
 static int const PATHWAYS = 8;
-static int const PAGE_SIZE = FILE_SIZE / PATHWAYS;
+// Tamanho do definição de estudante
+static int const STUDENT_SIZE = sizeof(Student);
+// Tamanho do arquivo final, que não tenha sobras
+static int const FILE_SIZE = MIB*SIZE - (((MIB*SIZE)/PATHWAYS)%STUDENT_SIZE)*PATHWAYS;
+// Numero de regitros
+static int const REG_NUM = FILE_SIZE/STUDENT_SIZE;
+// Tamanho das páginas de registros
+static int const REG_PAGE_SIZE = REG_NUM / PATHWAYS;
 
 int main(int argc, char *argv[]) {
 
@@ -29,32 +39,28 @@ int main(int argc, char *argv[]) {
      * Descobre quantos registros são necessário para atingir o tamanho
      * tamanho em Mebibytes
      */
-    long regNum = FILE_SIZE /sizeof(Student);
-    cout << regNum << endl;
+    cout << REG_NUM << endl;
     // Calcula as iterações com base do numero de elementos e páginas
-    long iterations = regNum / PAGE_SIZE;
-    long remaining = regNum % PAGE_SIZE;
+    long iterations = REG_NUM / REG_PAGE_SIZE;
     StudentFactory *factory = new StudentFactory();
 
-    writeFile(f, iterations, remaining, factory);
+    writeFile(f, iterations, factory);
 
     rewind(f);
 
-    readFile(f, iterations, remaining);
+    readFile(f, iterations);
 
     return 0;
 }
 
-void readFile(FILE *f, long iterations, long remaining) {
+void readFile(FILE *f, long iterations) {
     time_t start,end;
     time (&start);
 
     /// Executa a gravação
     for (int i = 0; i < iterations; i++) {
-        readElements(f, PAGE_SIZE);
+        readElements(f, REG_PAGE_SIZE);
     }
-    /// Le o que sobrou dos registros
-    readElements(f, remaining);
 
     time (&end);
     cout << "Tempo de Leitura:" << end -start << endl;
@@ -62,18 +68,17 @@ void readFile(FILE *f, long iterations, long remaining) {
     fclose(f);
 }
 
-void writeFile(FILE *f, long iterations, long remaining, StudentFactory *factory) {
+void writeFile(FILE *f, long iterations, StudentFactory *factory) {
     time_t start,end;
     time (&start);
 
     /// Executa a gravação
     for (int i = 0; i < iterations; i++) {
-        writeElements(f, PAGE_SIZE, factory);
+        writeElements(f, REG_PAGE_SIZE, factory);
     }
-    /// Grava o resto da quantidade de registros
-    writeElements(f, remaining, factory);
 
     time (&end);
+
     cout << "Tempo de Gravação:" << difftime (end,start) << endl;
 }
 
