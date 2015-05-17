209a726ae7b16702e3fc8ba1eecb8f195e498367
Bruno Dix <brunodix@gmail.com>
2015-05-17 15:52:59 -0300

    Alterações na arvoree.

--- ARVORE_B/main.cpp
+++ ARVORE_B/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	BTree *btree = new BTree();
+	BTree *btree = new BTree(2);
 	btree->insert(8);
 	btree->insert(3);
 	btree->insert(1);
@@ -11,5 +11,23 @@ int main(int argc, char *argv[]) {
 	btree->insert(5);
 	btree->insert(4);
 	btree->insert(9);
+	btree->insert(56);
+	btree->insert(78);
+	btree->insert(34);
+	btree->insert(87);
+	btree->insert(23);
+	btree->insert(90);
+	btree->traverse();
+	cout << endl;
+
+	btree->remove(90);
+	btree->traverse();
+	cout << endl;
+	btree->remove(56);
+	btree->traverse();
+	cout << endl;
+	btree->remove(99);
+	btree->traverse();
+	cout << endl;
 }
 
