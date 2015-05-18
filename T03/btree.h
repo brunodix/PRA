2c9eb7168922bd5ae723572857df9d5565068149
Bruno Dix <brunodix@gmail.com>
2015-05-18 01:54:48 -0300

    Alterações na ordenação dos registros.

--- T03/btree.h
+++ T03/btree.h
@@ -9,127 +9,150 @@ using namespace std;
 
 class Key {
 	private:
-		Student *student;
+		Student *student = NULL;
 	public:
-		virtal int compare(Key k);
-};
+		Key(Student *student) {
+			this->student = student;
+		};
 
-class NameKey: public Key {
-	public:
-		int compare(Key k) {
-			if (student->getName() == k.student->getName()) {
-				return 0;
-			} else if (student->getName() < k.student->getName()) {
-				return -1;
-			}
+		long traverse() {
+			return student->getEnrollNumber();
 		}
-
+		void init() {
+			student = NULL;
+		};
+		Student *getStudent(){ return student; };
 };
 
 class BTreeNode {
-	int *keys;  // An array of keys
-	int t;      // Minimum degree (defines the range for number of keys)
-	BTreeNode **child; // An array of child pointers
-	int n;     // Current number of keys
-	bool leaf; // Is true when node is leaf. Otherwise false
+    Key **keys;  // An array of keys
+    int t;      // Minimum degree (defines the range for number of keys)
+    BTreeNode **child; // An array of child pointers
+    int n;     // Current number of keys
+    bool leaf; // Is true when node is leaf. Otherwise false
+	int (*fComparator)(const void *, const void *);
 
 public:
-	BTreeNode(int _t, bool _leaf);   // Constructor
+    BTreeNode(int _t, bool _leaf, int comparator(const void *, const void *));   // Constructor
 
-	// A utility function to insert a new key in the subtree rooted with
-	// this node. The assumption is, the node must be non-full when this
-	// function is called
-	void insertNonFull(int k);
+    // A utility function to insert a new key in the subtree rooted with
+    // this node. The assumption is, the node must be non-full when this
+    // function is called
+    void insertNonFull(Key *k);
 
-	// A utility function to split the child y of this node. i is index of y in
-	// child array C[].  The Child y must be full when this function is called
-	void splitChild(int i, BTreeNode *y);
+    // A utility function to split the child y of this node. i is index of y in
+    // child array C[].  The Child y must be full when this function is called
+    void splitChild(int i, BTreeNode *y);
 
-	// A function to traverse all nodes in a subtree rooted with this node
-	void traverse();
+    // A function to traverse all nodes in a subtree rooted with this node
+    void traverse();
 
-	// A function to search a key in subtree rooted with this node.
-	BTreeNode *search(int k);   // returns NULL if k is not present.
+	void traverse(FILE *f);
 
-	// Make BTree friend of this so that we can access private members of this
-	// class in BTree functions
-	friend class BTree;
+    // Make BTree friend of this so that we can access private members of this
+    // class in BTree functions
+    friend class BTree;
 
-	int getPred(int idx);
-
-	void removeFromLeaf(int idx);
-
-	void remove(int k);
-
-	int findKey(int k);
-
-	void removeFromNonLeaf(int idx);
-
-	int getSucc(int idx);
-
-	void merge(int idx);
-
-	void borrowFromPrev(int idx);
-
-	void borrowFromNext(int idx);
-
-	void fill(int idx);
+//	int getPred(int idx);
+//
+//	void removeFromLeaf(int idx);
+//
+//	void remove(int k);
+//
+//	int findKey(int k);
+//
+//	void removeFromNonLeaf(int idx);
+//
+//	int getSucc(int idx);
+//
+//	void merge(int idx);
+//
+//	void borrowFromPrev(int idx);
+//
+//	void borrowFromNext(int idx);
+//
+//	void fill(int idx);
 };
 
 // A BTree
-class BTree
-{
-	BTreeNode *root; // Pointer to root node
-	int t;  // Minimum degree
+class BTree {
+    BTreeNode *root; // Pointer to root node
+    int t;  // Minimum degree
+	int (*fComparator)(const void *, const void *);
 
 public:
-	// Constructor (Initializes tree as empty)
-	BTree(int _t)
-	{  root = NULL;  t = _t; }
+    // Constructor (Initializes tree as empty)
+    BTree(int _t, int comparator(const void *, const void *)) {
+        root = NULL;
+        t = _t;
+        fComparator = comparator;
+    }
+
+    // function to traverse the tree
+    void traverse() { if (root != NULL) root->traverse(); }
 
 	// function to traverse the tree
-	void traverse()
-	{  if (root != NULL) root->traverse(); }
+	void traverse(FILE *f) { if (root != NULL) root->traverse(f); }
 
-	// function to search a key in this tree
-	BTreeNode* search(int k)
-	{  return (root == NULL)? NULL : root->search(k); }
+    // The main function that inserts a new key in this B-Tree
+    void insert(Key *k);
 
-	// The main function that inserts a new key in this B-Tree
-	void insert(int k);
+    void remove(int k);
 
-	void remove(int k);
+	friend class BTreeNode;
+
+	void traverseToFile(FILE *pFILE);
 };
 
 // Constructor for BTreeNode class
-BTreeNode::BTreeNode(int t1, bool leaf1)
-{
-	// Copy the given minimum degree and leaf property
-	t = t1;
-	leaf = leaf1;
+BTreeNode::BTreeNode(int t1, bool leaf1, int comparator(const void *, const void *)) {
+    // Copy the given minimum degree and leaf property
+    t = t1;
+    leaf = leaf1;
 
-	// Allocate memory for maximum number of possible keys
-	// and child pointers
-	keys = new int[2*t-1];
-	child = new BTreeNode *[2*t];
+    // Allocate memory for maximum number of possible keys
+    // and child pointers
+    keys = new Key *[2 * t - 1];
+    child = new BTreeNode *[2 * t];
+	for (int i = 0; i < 2*t-1; i++) {
+		keys[i]=NULL;
+	}
 
-	// Initialize the number of keys as 0
-	n = 0;
+    // Initialize the number of keys as 0
+    n = 0;
+	fComparator = comparator;
 }
 
 // Function to traverse all nodes in a subtree rooted with this node
-void BTreeNode::traverse()
-{
+void BTreeNode::traverse(FILE *f) {
+    // There are n keys and n+1 children, travers through n keys
+    // and first n children
+    int i;
+    for (i = 0; i < n; i++) {
+        // If this is not leaf, then before printing key[i],
+        // traverse the subtree rooted with child C[i].
+        if (leaf == false)
+            child[i]->traverse(f);
+		long l = keys[i]->traverse();
+		fwrite(&l, sizeof(long), 1, f);
+    }
+
+    // Print the subtree rooted with last child
+    if (leaf == false)
+        child[i]->traverse(f);
+}
+
+// Function to traverse all nodes in a subtree rooted with this node
+void BTreeNode::traverse() {
 	// There are n keys and n+1 children, travers through n keys
 	// and first n children
 	int i;
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++) {
 		// If this is not leaf, then before printing key[i],
 		// traverse the subtree rooted with child C[i].
 		if (leaf == false)
 			child[i]->traverse();
-		cout << " " << keys[i];
+		cout << " " << keys[i]->traverse();
 	}
 
 	// Print the subtree rooted with last child
@@ -137,67 +160,129 @@ void BTreeNode::traverse()
 		child[i]->traverse();
 }
 
-// Function to search key k in subtree rooted with this node
-BTreeNode *BTreeNode::search(int k)
-{
-	// Find the first key greater than or equal to k
-	int i = 0;
-	while (i < n && k > keys[i])
-		i++;
-
-	// If the found key is equal to k, return this node
-	if (keys[i] == k)
-		return this;
-
-	// If key is not found here and this is a leaf node
-	if (leaf == true)
-		return NULL;
-
-	// Go to the appropriate child
-	return child[i]->search(k);
-}
-
 // The main function that inserts a new key in this B-Tree
-void BTree::insert(int k, Student *value)
-{
-	// If tree is empty
-	if (root == NULL)
-	{
-		// Allocate memory for root
-		root = new BTreeNode(t, true);
-		root->keys[0] = k;  // Insert key
-		root->n = 1;  // Update number of keys in root
-	}
-	else // If tree is not empty
-	{
-		// If root is full, then tree grows in height
-		if (root->n == 2*t-1)
-		{
-			// Allocate memory for new root
-			BTreeNode *s = new BTreeNode(t, false);
+void BTree::insert(Key *k) {
+    // If tree is empty
+    if (root == NULL) {
+        // Allocate memory for root
+        root = new BTreeNode(t, true, fComparator);
+        root->keys[0] = k;  // Insert key
+        root->n = 1;  // Update number of keys in root
+    }
+    else // If tree is not empty
+    {
+        // If root is full, then tree grows in height
+        if (root->n == 2 * t - 1) {
+            // Allocate memory for new root
+            BTreeNode *s = new BTreeNode(t, false, fComparator);
 
-			// Make old root as child of new root
-			s->child[0] = root;
+            // Make old root as child of new root
+            s->child[0] = root;
 
-			// Split the old root and move 1 key to the new root
-			s->splitChild(0, root);
+            // Split the old root and move 1 key to the new root
+            s->splitChild(0, root);
 
-			// New root has two children now.  Decide which of the
-			// two children is going to have new key
-			int i = 0;
-			if (s->keys[0] < k)
-				i++;
-			s->child[i]->insertNonFull(k);
+            // New root has two children now.  Decide which of the
+            // two children is going to have new key
+            int i = 0;
+			//if (s->keys[0] < k)
+            if (fComparator(k, s->keys[0]) > 0 )
+                i++;
+            s->child[i]->insertNonFull(k);
 
-			// Change root
-			root = s;
-		}
-		else  // If root is not full, call insertNonFull for root
-			root->insertNonFull(k);
-	}
+            // Change root
+            root = s;
+        }
+        else  // If root is not full, call insertNonFull for root
+            root->insertNonFull(k);
+    }
 }
 
+// A utility function to split the child y of this node
+// Note that y must be full when this function is called
+void BTreeNode::splitChild(int i, BTreeNode *y) {
+    // Create a new node which is going to store (t-1) keys
+    // of y
+    BTreeNode *z = new BTreeNode(y->t, y->leaf, fComparator);
+    z->n = t - 1;
 
+    // Copy the last (t-1) keys of y to z
+    for (int j = 0; j < t - 1; j++)
+        z->keys[j] = y->keys[j + t];
+
+    // Copy the last t children of y to z
+    if (y->leaf == false) {
+        for (int j = 0; j < t; j++)
+            z->child[j] = y->child[j + t];
+    }
+
+    // Reduce the number of keys in y
+    y->n = t - 1;
+
+    // Since this node is going to have a new child,
+    // create space of new child
+    for (int j = n; j >= i + 1; j--)
+        child[j + 1] = child[j];
+
+    // Link the new child to this node
+    child[i + 1] = z;
+
+    // A key of y will move to this node. Find location of
+    // new key and move all greater keys one space ahead
+    for (int j = n - 1; j >= i; j--)
+        keys[j + 1] = keys[j];
+
+    // Copy the middle key of y to this node
+    keys[i] = y->keys[t - 1];
+
+    // Increment count of keys in this node
+    n = n + 1;
+}
+
+// A utility function to insert a new key in this node
+// The assumption is, the node must be non-full when this
+// function is called
+void BTreeNode::insertNonFull(Key *k) {
+    // Initialize index as index of rightmost element
+    int i = n - 1;
+
+    // If this is a leaf node
+    if (leaf == true) {
+        // The following loop does two things
+        // a) Finds the location of new key to be inserted
+        // b) Moves all greater keys to one place ahead
+		//while (i >= 0 && keys[i] > k)
+        while (i >= 0 && fComparator(keys[i], k) > 0) {
+            keys[i + 1] = keys[i];
+            i--;
+        }
+
+        // Insert the new key at found location
+        keys[i + 1] = k;
+        n = n + 1;
+    }
+    else // If this node is not leaf
+    {
+        // Find the child which is going to have the new key
+        while (i >= 0 && fComparator(keys[i], k) > 0)
+            i--;
+
+        // See if the found child is full
+        if (child[i + 1]->n == 2 * t - 1) {
+            // If the child is full, then split it
+            splitChild(i + 1, child[i + 1]);
+
+            // After split, the middle key of C[i] goes up and
+            // C[i] is splitted into two.  See which of the two
+            // is going to have the new key
+            if (keys[i + 1] < k)
+                i++;
+        }
+        child[i + 1]->insertNonFull(k);
+    }
+}
+
+/*
 // A utility function that returns the index of the first key that is
 // greater than or equal to k
 int BTreeNode::findKey(int k)
@@ -492,96 +577,6 @@ void BTreeNode::merge(int idx)
 //	p´aginas irm˜as (rota¸c˜ao da sub-´arvore)
 //	2.2.2 caso contr´ario: concatena¸c˜ao recursiva entre p´aginas irm˜as (join)
 
-
-// A utility function to insert a new key in this node
-// The assumption is, the node must be non-full when this
-// function is called
-void BTreeNode::insertNonFull(int k)
-{
-	// Initialize index as index of rightmost element
-	int i = n-1;
-
-	// If this is a leaf node
-	if (leaf == true)
-	{
-		// The following loop does two things
-		// a) Finds the location of new key to be inserted
-		// b) Moves all greater keys to one place ahead
-		while (i >= 0 && keys[i] > k)
-		{
-			keys[i+1] = keys[i];
-			i--;
-		}
-
-		// Insert the new key at found location
-		keys[i+1] = k;
-		n = n+1;
-	}
-	else // If this node is not leaf
-	{
-		// Find the child which is going to have the new key
-		while (i >= 0 && keys[i] > k)
-			i--;
-
-		// See if the found child is full
-		if (child[i+1]->n == 2*t-1)
-		{
-			// If the child is full, then split it
-			splitChild(i+1, child[i+1]);
-
-			// After split, the middle key of C[i] goes up and
-			// C[i] is splitted into two.  See which of the two
-			// is going to have the new key
-			if (keys[i+1] < k)
-				i++;
-		}
-		child[i+1]->insertNonFull(k);
-	}
-}
-
-// A utility function to split the child y of this node
-// Note that y must be full when this function is called
-void BTreeNode::splitChild(int i, BTreeNode *y)
-{
-	// Create a new node which is going to store (t-1) keys
-	// of y
-	BTreeNode *z = new BTreeNode(y->t, y->leaf);
-	z->n = t - 1;
-
-	// Copy the last (t-1) keys of y to z
-	for (int j = 0; j < t-1; j++)
-		z->keys[j] = y->keys[j+t];
-
-	// Copy the last t children of y to z
-	if (y->leaf == false)
-	{
-		for (int j = 0; j < t; j++)
-			z->child[j] = y->child[j+t];
-	}
-
-	// Reduce the number of keys in y
-	y->n = t - 1;
-
-	// Since this node is going to have a new child,
-	// create space of new child
-	for (int j = n; j >= i+1; j--)
-		child[j+1] = child[j];
-
-	// Link the new child to this node
-	child[i+1] = z;
-
-	// A key of y will move to this node. Find location of
-	// new key and move all greater keys one space ahead
-	for (int j = n-1; j >= i; j--)
-		keys[j+1] = keys[j];
-
-	// Copy the middle key of y to this node
-	keys[i] = y->keys[t-1];
-
-	// Increment count of keys in this node
-	n = n + 1;
-}
-
 void BTree::remove(int k)
 {
 	if (!root)
@@ -607,6 +602,11 @@ void BTree::remove(int k)
 		delete tmp;
 	}
 	return;
+}*/
+
+void BTree::traverseToFile(FILE *pFILE) {
+	if (root != NULL) root->traverse(pFILE);
+
 }
 
 #endif //BTREE_H
--- T03/main.cpp
+++ T03/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include "student.h"
 #include "student_factory.h"
-#include <stdio.h>
-#include <iostream>
 #include <ctime>
+#include "btree.h"
 
 
-void writeElements(FILE *f, long size, StudentFactory *factory);
+void writeElements(FILE *f, long size, StudentFactory *factory, BTree *btree);
 
 void readElements(FILE *f, long size);
+
+void writeIndex(BTree *pTree);
+
+int comparatorName(const void *key1, const void *key2) {
+    Student *s1 = ((Key *) key1)->getStudent();
+    Student *s2 = ((Key *) key2)->getStudent();
+    return s1->getName().compare(s2->getName());
+}
 
 using namespace std;
 
 
+
 int main(int argc, char *argv[]) {
 
+
+    BTree *btree = new BTree(8, comparatorName);
     if (argc != 3) {
        cout << "Informe os parâmetros: <tamanho da pagina> <tamanho em Mib>" << endl;
        return 1;
@@ -46,13 +56,15 @@ int main(int argc, char *argv[]) {
 
     /// Executa a gravação
     for (int i = 0; i < iterations; i++) {
-        writeElements(f, pageSize, factory);
+        writeElements(f, pageSize, factory, btree);
     }
     /// Grava o resto da quantidade de registros
-    writeElements(f, remaining, factory);
+    writeElements(f, remaining, factory, btree);
+
+    writeIndex(btree);
 
     time (&end);
-    cout << "Tempo de Gravação:" << difftime (end,start) << endl;
+    cout << "Tempo de Gravação:" << difftime (end, start) << endl;
 
     rewind(f);
 
@@ -66,18 +78,24 @@ int main(int argc, char *argv[]) {
     readElements(f, remaining);
 
     time (&end);
-    cout << "Tempo de Leitura:" << end -start << endl;
+    cout << "Tempo de Leitura:" << end-start << endl;
 
     fclose(f);
 
     return 0;
 }
 
-void writeElements(FILE *f, long size, StudentFactory *factory) {
+void writeIndex(BTree *pTree) {
+    FILE *file = fopen("index.bin", "wb");
+    pTree->traverseToFile(file);
+}
+
+void writeElements(FILE *f, long size, StudentFactory *factory, BTree *btree) {
     for (int i = 0; i < size; i++) {
         Student *student = factory->getStochastic();
         fwrite(student, sizeof(Student), 1, f);
-        delete(student);
+        btree->insert(new Key(student));
+        //delete(student);
     }
     fflush(f);
 }
